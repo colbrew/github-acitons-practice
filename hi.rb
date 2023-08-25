b8505fb3160b68994449bce065c532f188c9e405
Daniel Colman <33821190+colbrew@users.noreply.github.com>
2023-08-25 15:55:03 -0400

    Update hi.rb

--- hi.rb
+++ hi.rb
@@ -1,3 +1,9 @@
 # frozen string literal: true
 
-puts "hi there"
+require 'fileutils'
+require 'json'
+
+file = File.read('./README.md')
+puts "The README doc say: #{file}"
+file << ' and you'
+File.write('./README.md', file)
